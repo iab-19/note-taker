e6e4ba23c85ccb44216f0fafbf4a6075c7691cc4
Ibrahima <iab.jr10@gmail.com>
2023-08-19 02:51:22 -0400

    Add todos for a get method and a post method

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const PORT = 2023;
 
 app.use(express.static('public')); // Point static middleware to the public folder
 
-app.get('/', (req, res) => res.send('Navigate to /notes')); // Navigate to index.html
+// Navigate to index.html
+app.get('/', (req, res) => res.send('Navigate to /notes'));
 
+// Navigate to notes.html
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/notes.html')) // Navigate to notes.html
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// Reroute to index.html whenever an invalid URL is entered
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html')); // Reroute to index.html whenever an invalid URL is entered
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+// TODO: Read the db.json file and return all saved notes as JSON at the route GET/api/notes
+
+
+// TODO: Recive a new note to save on the request body, add it to the db.json file, and return
+// the new note to the client at the route POST/api.notes
 
 // Initialize server
 app.listen(PORT, () =>
